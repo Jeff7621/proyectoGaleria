a6e4d9b524f36f10a728b236df684b387b75af57
Jefferson González <jefferg675@gmail.com>
2024-09-12 15:54:20 -0600

    Se implemeto la tabla de usuarios

---
+++ Usuario.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoGaleria
+{
+    internal class Usuario
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Unique, NotNull]
+        public string NombreUsuario { get; set; }//Nombre de usuario
+
+        [Unique, NotNull]
+        public string Email { get; set; }//Email de usuario
+
+        [Unique]
+        public string Contraseña { get; set; }//Contraseño de usuario
+    }
+}
